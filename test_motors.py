762056cf895e1a9872cacdfd703142dc8e7537a3
Mary Karapetyan <mekarape@usc.edu>
2026-05-07 20:28:08 -0700

    Fix motor_test.py

--- test_motors.py
+++ test_motors.py
@@ -5,7 +5,7 @@ print("Testing motors...")
 motors = Motors()
 
 print("Forward 2 seconds")
-motors._send_rpm(800, 800)
+motors.set_speed(0.3, 0.3)
 time.sleep(2)
 
 print("Stop")
@@ -13,7 +13,7 @@ motors.stop()
 time.sleep(1)
 
 print("Turn right 2 seconds")
-motors._send_rpm(800, -800)
+motors.set_speed(0.3, -0.3)
 time.sleep(2)
 
 print("Stop")
